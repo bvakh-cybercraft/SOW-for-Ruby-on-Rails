5eed80ce91ed69852da80e0a4166d71688da6042
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-01-23 17:52:55 +0200

    feat/add enum status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :description, :due_date)
+    params.require(:task).permit(:title, :description, :due_date, :status)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  enum status: { active: 0, completed: 1 }
+
   validates :title, presence: true
   validates :description, presence: true
   validates :due_date, presence: true
