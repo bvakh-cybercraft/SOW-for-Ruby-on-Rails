31b172c6d7f70de5d34d1e06dc6c1409d9d4487b
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-01-23 13:39:16 +0200

    feat/add task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :delete, :destroy]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+
+    binding.pry
+
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit
+    end
+  end
+
+  def delete
+  end
+
+  def destroy
+
+    @task.destroy
+  end
+
+  private
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date)
+  end
+end
