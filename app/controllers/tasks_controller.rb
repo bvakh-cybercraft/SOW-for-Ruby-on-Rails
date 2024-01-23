d008ac8ef5a1b2848fd88e0abc38ec68b9bef0cd
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-01-23 19:18:13 +0200

    feat/add priority enum

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :description, :due_date, :status)
+    params.require(:task).permit(:title, :description, :due_date, :status, :priority)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   enum status: { active: 0, completed: 1 }
+  enum priority: { low: 0, medium: 1, high: 2 }
 
   validates :title, presence: true
   validates :description, presence: true
