59fd4d72a63171d1c897d87d68d2a97e51a774a8
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-01-23 19:17:45 +0200

    feat/add priority attribute to tasks table

--- db/migrate/20240123140524_add_columns_to_task.rb
+++ db/migrate/20240123140524_add_columns_to_task.rb
@@ -1,5 +1,6 @@
 class AddColumnsToTask < ActiveRecord::Migration[7.1]
   def change
     add_column :tasks, :status, :integer, default: 0
+    add_column :tasks, :priority, :integer, default: 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_140524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.integer "priority", default: 1
   end
 
 end
