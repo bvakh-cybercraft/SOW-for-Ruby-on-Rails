05496467f20d0f0d471d5e68161c0a545247f81a
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-01-24 14:20:29 +0200

    feat/add validations for tasks table

--- db/migrate/20240123110411_create_tasks.rb
+++ db/migrate/20240123110411_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.string :description
-      t.date :due_date
+      t.string :title, null: false, limit: 120
+      t.string :description, null: false, limit: 200
+      t.date :due_date, null: false
 
       t.timestamps
     end
