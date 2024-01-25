176e1122b2f17eb8babba36c059b8df2a7711e19
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-01-25 10:19:59 +0200

    feat/add helpers for task status and priority

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,22 @@
 module TasksHelper
+  def priority_color(task)
+    case task.priority.to_sym
+    when :low
+      'bg-secondary'
+    when :medium
+      'bg-warning'
+    when :high
+      'bg-danger'
+    else
+      'bg-danger'
+    end
+  end
+
+  def active_check(task)
+    case task.status.to_sym
+    when :active
+      'bg-success'
+    when :completed
+      'bg-secondary'
+  end
 end
