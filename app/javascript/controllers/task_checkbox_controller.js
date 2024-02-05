aa7139f3a80eeb757fd9e5b9795783959d201360
bvakh-cybercraft <bvakh@cybercraftinc.com>
2024-02-05 16:52:21 +0200

    feat/add triggerNameChange function

--- app/javascript/controllers/task_checkbox_controller.js
+++ app/javascript/controllers/task_checkbox_controller.js
@@ -3,6 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="task-checkbox"
 export default class extends Controller {
   connect() {
+    const inputField = this.element.querySelector("input[type='text']");
+    const updateButton = this.element.querySelector(".btn-primary");
+    const initialValue = inputField.value;
+
+    inputField.addEventListener("input", () => {
+      const hasChanges = inputField.value !== initialValue;
+      console.log(hasChanges)
+      updateButton.style.display = hasChanges ? "block" : "none";
+    });
+
+    updateButton.addEventListener("click", () => {
+      updateButton.style.display = "none";
+    });
   }
 
   mark_as_completed(e) {
@@ -49,6 +62,34 @@ export default class extends Controller {
           console.log("Task Checkbox Updated");
         } else {
           console.error("Failed to update task checkbox");
+        }
+      })
+  }
+
+  triggerNameChange(e) {
+    e.preventDefault();
+
+    const taskCheckbox = this.element;
+    const taskCheckboxId = taskCheckbox.id;
+    const nameInput = taskCheckbox.querySelector("input[type='text']");
+    const newName = nameInput.value;
+
+    fetch(`task_checkboxes/${taskCheckboxId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content
+      },
+      body: JSON.stringify({
+        name: newName
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === "success") {
+          console.log("Task Checkbox Name Updated");
+        } else {
+          console.error("Failed to update task checkbox name");
         }
       })
   }
