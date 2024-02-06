1bc0654a9e336c17f5e82480424822a7cfc720ca
Bohdan Vakh <157469014+bvakh-cybercraft@users.noreply.github.com>
2024-02-06 10:47:48 +0200

    Revert "Feat/add tast checkboxes"

--- db/migrate/20240202122726_add_task_checkbox_to_task.rb
+++ db/migrate/20240202122726_add_task_checkbox_to_task.rb
@@ -2,7 +2,7 @@ class AddTaskCheckboxToTask < ActiveRecord::Migration[7.1]
   def change
     create_table :task_checkboxes do |t|
       t.string :name, null: false, limit: 120
-      t.boolean :checked, null: false, default: false
+      t.boolean :checked, null: false
     end
   end
 end
