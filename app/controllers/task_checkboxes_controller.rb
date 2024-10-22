4dae66372e0f2ab9121d0f6bf328606bebcfea44
Pavlo Roman <proman@cybercraftinc.com>
2024-10-22 17:29:48 +0300

    Comment task_checkbox_params method

--- app/controllers/task_checkboxes_controller.rb
+++ app/controllers/task_checkboxes_controller.rb
@@ -14,14 +14,14 @@ class TaskCheckboxesController < ApplicationController
     @task_checkbox.destroy
     respond_to do |format|
       format.html { redirect_to task_path(@task_id), notice: "task checkbox was successfully destroyed." }
-      format.json { head :no_content }
-      format.js
+      # format.json { head :no_content }
+      # format.js
     end
   end
 
   private
 
-  def task_checkbox_params
-    params.require(:task_checkbox).permit(:id, :name, :checked)
-  end
+  # def task_checkbox_params
+  #   params.require(:task_checkbox).permit(:id, :name, :checked)
+  # end
 end
